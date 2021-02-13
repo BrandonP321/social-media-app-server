a11647dbabc8a44992336b30da022c3e0167037e
Brandon Phillips <brandon.phillips976@gmail.com>
2021-02-12 19:53:08 -0800

    created Post schema

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: 'Creator require'
+    },
+    img: {
+        type: String,
+        trim: true,
+        required: 'Image required'
+    },
+    caption: {
+        type: String,
+        trim: true,
+        required: false
+    },
+    likesAmount: {
+        type: Number,
+        default: 0
+    },
+    usersWhoLike: {
+        type: Array,
+        default: []
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Post = mongoose.model("Post", schema)
+
+module.exports = Post;
