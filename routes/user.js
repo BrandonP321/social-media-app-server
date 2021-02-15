8acac5363da266dcb8f63be222a43f963f7478d2
Brandon Phillips <brandon.phillips976@gmail.com>
2021-02-14 16:11:21 -0800

    added login route

--- routes/user.js
+++ routes/user.js
@@ -14,13 +14,13 @@ router.post('/user/create', (req, res) => {
     db.User.findOne({ email: req.body.email }, (err, data) => {
         console.log(data)
         // if user found, email is taken; send status 409
-        if (data) return res.status(409).send('Email taken')
+        if (data) return res.status(409).send('Email taken').end();
 
         // else check for any user with same username
         db.User.findOne({ username: req.body.username }, (err, data) => {
             console.log(data)
             // if user found, username is taken; send status 422
-            if (data) return res.status(422).send("Username taken")
+            if (data) return res.status(422).send("Username taken").end();
 
             // else create new document in database for user
             db.User.create(req.body, (err, data) => {
@@ -31,11 +31,20 @@ router.post('/user/create', (req, res) => {
 })
 
 router.post('/user/login', (req, res) => {
-    try {
-        
-    } catch (error) {
-        res.status(500).send("Server error while logging in")
-    }
+    console.log(req.body)
+    db.User.findOne({ email: req.body.email }, async (err, user) => {
+        console.log(user)
+        // if no user found, send status 401 for incorrect email or password
+        if (!user) return res.status(401).send("Incorrect email or password").end();
+
+        // validate password matches stored encrypted password
+        const isValidPassword = await user.validatePassword(req.body.password)
+        // if passwords don't match, send status 401 for incorrect email or password
+        if (!isValidPassword) return res.status(401).send("Incorrect email or password").end();
+
+        // if passwords match, send user id and jwt to client
+        res.json(user._id).end();
+    })
 })
 
 module.exports = router
